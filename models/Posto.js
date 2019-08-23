715cdc14d8315878e31431a2f903f857f6b46b9f
Joseh <josehaugustodsp@gmail.com>
2019-08-23 17:05:52 -0300

    /Postos POST and validation init

---
+++ models/Posto.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const PostoSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    address: {
+        type: String,
+        required: true
+    },
+    gasprice: {
+        type: Number,
+        required: false
+    }
+});
+
+module.exports = mongoose.model('posto', PostoSchema);
--- routes/postos.js
+++ routes/postos.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const { check, validationResult } = require('express-validator');
+const Posto = require('../models/Posto');
 
 // @route       GET api/postos
 // @desc        Pegar os postos
@@ -11,8 +13,39 @@ router.get('/', (req, res) => {
 // @route       POST api/postos
 // @desc        Cadastrar um posto
 // @access      Public
-router.post('/', (req, res) => {
-    res.send('Cadastrar Posto');
+router.post('/',
+[
+    check('name', 'name is required').not().isEmpty(),
+    check('address', 'address is required').not().isEmpty(),
+], async (req, res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        return res.status(400).json({errors: errors.array()});
+    }
+
+    const { id, name, address, gasprice} = req.body;
+
+    try {
+        let posto = await Posto.findOne({ id });
+
+        if(posto){
+            return res.status(400).json({msg: 'O posto ja existe'});
+        }
+        
+        posto = new Posto({
+            id,
+            name,
+            address,
+            gasprice
+        });
+
+        await posto.save();
+
+        res.send("Posto adicionado");
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
 });
 
 // @route       PUT api/postos
