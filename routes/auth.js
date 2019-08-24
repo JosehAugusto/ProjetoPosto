470931003d51660f531c87a100c68ce7b735115a
Joseh <josehaugustodsp@gmail.com>
2019-08-23 21:55:08 -0300

    Login com token de autentificação

--- routes/auth.js
+++ routes/auth.js
@@ -3,9 +3,22 @@ const router = express.Router();
 const bcryp = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const config = require('config');
+const auth = require('../middleware/auth');
 const { check, validationResult } = require('express-validator');
 const User = require('../models/User');
 
+// @route       GET api/users
+// @desc        pegar usuario logado
+// @access      Private
+router.get('/', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select('-password');
+        res.json(user);
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
 
 // @route       POST api/users
 // @desc        Logar um user
@@ -48,11 +61,13 @@ router.post('/',
             if(err) throw err;
             res.json({token});
         });
-        
+
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error');
     }
 });
+
+
 
 module.exports = router;
