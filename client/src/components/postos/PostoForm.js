b3d20c990b5a891251cca6177fdb40ac06ef88a7
Lucas Novaes <lucasmsn200@gmail.com>
2019-08-24 20:56:20 -0300

    novo posto sendo adicionado

--- client/src/components/postos/PostoForm.js
+++ client/src/components/postos/PostoForm.js
@@ -1,11 +1,145 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { addPosto, getPostos } from '../../actions/postoActions';
 
-const PostoForm = () => {
+let PostoForm = ({ addPosto, getPostos }) => {
+
+  let [name, setName] = useState('');
+  let [address, setAddress] = useState('');
+  let [gas_com_price, setGasComPrice] = useState('');
+  let [gas_adi_price, setGasAdiPrice] = useState('');
+  let [eta_price, setEtaPrice] = useState('');
+  let [gnvf_price, setGnvfPrice] = useState('');
+  let [die_price, setDiePrice] = useState('');
+  let [alc_price, setAlcPrice] = useState('');
+
+  let onSubmit = () => {
+    if (name === '' || address === '' || gas_com_price === '' || gas_adi_price === '' || eta_price === '' || gnvf_price === '' || die_price === '' || alc_price === '') {
+      // Favor preencher todos os campos
+    } else {
+
+      const gasolina_comun_price = [gas_com_price];
+      const gasolina_aditivada_price = [gas_adi_price];
+      const etanol_price = [eta_price];
+      const gnv_price = [gnvf_price];
+      const diesel_price = [die_price];
+      const alcool_price = [alc_price];
+
+
+      addPosto({
+        name,
+        address,
+        gasolina_comun_price,
+        gasolina_aditivada_price,
+        etanol_price,
+        gnv_price,
+        diesel_price,
+        alcool_price
+      });
+
+      // Mensagem dizendo que um posto foi adicionado
+
+      // Clear Fields
+      setName('');
+      setAddress('');
+      setGasComPrice('');
+      setGasAdiPrice('');
+      setEtaPrice('');
+      setGnvfPrice('');
+      setDiePrice('');
+      setAlcPrice('');
+    }
+  };
+
+
   return (
-    <div>
-
-    </div>
+    <form className="py-2 ">
+      <div className="text-center">
+        <h5 className="display-5"> Novo Posto </h5>
+      </div>
+      <div className="form-group">
+        <label className="font-weight-normal">Nome</label>
+        <input type="text" name='name' value={name} onChange={e => setName(e.target.value)} className="form-control"
+          placeholder="Digite o nome do posto..." />
+      </div>
+      <div className="form-group">
+        <label>Endereço</label>
+        <input type="text" name='address' value={address} onChange={e => setAddress(e.target.value)} className="form-control" placeholder="Digite o endereço do posto..." />
+      </div>
+      <div className="d-flex justify-content-between">
+        <div className="form-group">
+          <label>Gasolina Comum</label>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <span className="input-group-text">R$</span>
+            </div>
+            <input type="number" name='gas_com_price' value={gas_com_price} onChange={e => setGasComPrice(e.target.value)} className="form-control number-input" min="0" />
+          </div>
+        </div>
+        <div className="form-group">
+          <label>Alcool</label>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <span className="input-group-text">R$</span>
+            </div>
+            <input type="number" name='alc_price' value={alc_price} onChange={e => setAlcPrice(e.target.value)} className="form-control number-input" min="0" />
+          </div>
+        </div>
+        <div className="form-group">
+          <label>Etanol</label>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <span className="input-group-text">R$</span>
+            </div>
+            <input type="number" name='eta_price' value={eta_price} onChange={e => setEtaPrice(e.target.value)} className="form-control number-input" min="0" />
+          </div>
+        </div>
+      </div>
+      <div className="d-flex justify-content-between">
+        <div className="form-group">
+          <label>Diesel</label>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <span className="input-group-text">R$</span>
+            </div>
+            <input type="number" name='die_price' value={die_price} onChange={e => setDiePrice(e.target.value)} className="form-control number-input" min="0" />
+          </div>
+        </div>
+        <div className="form-group">
+          <label>Gasolina Aditivada</label>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <span className="input-group-text">R$</span>
+            </div>
+            <input type="number" name='gas_adi_price' value={gas_adi_price} onChange={e => setGasAdiPrice(e.target.value)} className="form-control number-input" min="0" />
+          </div>
+        </div>
+        <div className="form-group">
+          <label>GNV</label>
+          <div className="input-group">
+            <div className="input-group-prepend">
+              <span className="input-group-text">R$</span>
+            </div>
+            <input type="number" name='gnvf_price' value={gnvf_price} onChange={e => setGnvfPrice(e.target.value)} className="form-control number-input" min="0" />
+          </div>
+        </div>
+      </div>
+      <div className="text-center my-2">
+        <a href='#!' onClick={onSubmit} className='btn btn-dark'>
+          <i className="fas fa-plus-circle mr-2"></i>Adicionar
+        </a>
+      </div>
+    </form>
   )
 }
 
-export default PostoForm
+PostoForm.propTypes = {
+  addPosto: PropTypes.func.isRequired,
+  getPostos: PropTypes.func.isRequired
+};
+
+export default connect(
+  null,
+  { addPosto, getPostos }
+)(PostoForm);
--- routes/postos.js
+++ routes/postos.js
@@ -8,49 +8,49 @@ var cron = require('node-cron');
 //Call updateAllGasPrices() every day
 cron.schedule('*/10 * * * * *', async () => {
     let postos = await Posto.find();
-   postos.forEach(updateAllGasPrices);
+    postos.forEach(updateAllGasPrices);
 });
 
 // Update all gas prices of the DB
-async function updateAllGasPrices(item){
+async function updateAllGasPrices(item) {
 
-    if(item.gasolina_comun_price.length >= 1){
+    if (item.gasolina_comun_price.length >= 1) {
         item.gasolina_comun_price.push(item.gasolina_comun_price[item.gasolina_comun_price.length - 1]);
-        if(item.gasolina_comun_price.length >= 31){
+        if (item.gasolina_comun_price.length >= 31) {
             item.gasolina_comun_price.shift();
         }
     }
-    if(item.gasolina_aditivada_price.length >= 1){
+    if (item.gasolina_aditivada_price.length >= 1) {
         item.gasolina_aditivada_price.push(item.gasolina_aditivada_price[item.gasolina_aditivada_price.length - 1]);
-        if(item.gasolina_aditivada_price.length >= 31){
+        if (item.gasolina_aditivada_price.length >= 31) {
             item.gasolina_aditivada_price.shift();
         }
     }
-    if(item.etanol_price.length >= 1){
+    if (item.etanol_price.length >= 1) {
         item.etanol_price.push(item.etanol_price[item.etanol_price.length - 1]);
-        if(item.etanol_price.length >= 31){
+        if (item.etanol_price.length >= 31) {
             item.etanol_price.shift();
         }
     }
-    if(item.gnv_price.length >= 1){
+    if (item.gnv_price.length >= 1) {
         item.gnv_price.push(item.gnv_price[item.gnv_price.length - 1]);
-        if(item.gnv_price.length >= 31){
+        if (item.gnv_price.length >= 31) {
             item.gnv_price.shift();
         }
     }
-    if(item.diesel_price.length >= 1){
+    if (item.diesel_price.length >= 1) {
         item.diesel_price.push(item.diesel_price[item.diesel_price.length - 1]);
-        if(item.diesel_price.length >= 31){
+        if (item.diesel_price.length >= 31) {
             item.diesel_price.shift();
         }
     }
-    if(item.alcool_price.length >= 1){
+    if (item.alcool_price.length >= 1) {
         item.alcool_price.push(item.alcool_price[item.alcool_price.length - 1]);
-        if(item.alcool_price.length >= 31){
+        if (item.alcool_price.length >= 31) {
             item.alcool_price.shift();
         }
     }
-    
+
     const gasPricesFields = {};
     gasPricesFields.gasolina_comun_price = item.gasolina_comun_price;
     gasPricesFields.gasolina_aditivada_price = item.gasolina_aditivada_price;
@@ -61,7 +61,7 @@ async function updateAllGasPrices(item){
 
     await Posto.findByIdAndUpdate(
         item.id,
-        {$set: gasPricesFields},
+        { $set: gasPricesFields },
     );
 }
 
@@ -82,84 +82,84 @@ router.get('/', async (req, res) => {
 // @desc        Cadastrar um posto
 // @access      Public
 router.post('/',
-[
-    check('name', 'name is required').not().isEmpty(),
-    check('address', 'address is required').not().isEmpty(),
+    [
+        check('name', 'name is required').not().isEmpty(),
+        check('address', 'address is required').not().isEmpty(),
 
-], async (req, res) => {
-    const errors = validationResult(req);
-    if(!errors.isEmpty()){
-        return res.status(400).json({errors: errors.array()});
-    }
-
-    const { name, address, gasolina_comun_price, gasolina_aditivada_price, etanol_price, gnv_price, diesel_price, alcool_price } = req.body;
-
-    try {
-        let posto = await Posto.findOne({ address });
-
-        if(posto){
-            return res.status(400).json({msg: 'O posto já existe'});
+    ], async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
         }
 
-        posto = new Posto({
-            name,
-            address,
-            gasolina_comun_price,
-            gasolina_aditivada_price,
-            etanol_price,
-            gnv_price,
-            diesel_price,
-            alcool_price
-        });
+        const { name, address, gasolina_comun_price, gasolina_aditivada_price, etanol_price, gnv_price, diesel_price, alcool_price } = req.body;
 
-        await posto.save();
+        try {
+            let posto = await Posto.findOne({ address });
 
-        res.send("Posto adicionado");
-    } catch (err) {
-        console.error(err.message);
-        res.status(500).send('Server Error');
-    }
-});
+            if (posto) {
+                return res.status(400).json({ msg: 'O posto já existe' });
+            }
+
+            posto = new Posto({
+                name,
+                address,
+                gasolina_comun_price,
+                gasolina_aditivada_price,
+                etanol_price,
+                gnv_price,
+                diesel_price,
+                alcool_price
+            });
+
+            await posto.save();
+
+            res.json(posto);
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+    });
 
 // @route       PUT api/postos
 // @desc        Editar informações de um posto
 // @access      Public
 router.put('/:id',
-[
-    check('name', 'name is required').not().isEmpty(),
-    check('address', 'address is required').not().isEmpty(),
-], async (req, res) => {
+    [
+        check('name', 'name is required').not().isEmpty(),
+        check('address', 'address is required').not().isEmpty(),
+    ], async (req, res) => {
 
-    const { name, address, gasolina_comun_price, gasolina_aditivada_price, etanol_price, gnv_price, diesel_price, alcool_price } = req.body;
+        const { name, address, gasolina_comun_price, gasolina_aditivada_price, etanol_price, gnv_price, diesel_price, alcool_price } = req.body;
 
-    const postoFields = {};
-    if(name) postoFields.name = name;
-    if(address) postoFields.address = address;
-    if(gasolina_comun_price) postoFields.gasolina_comun_price = gasolina_comun_price;
-    if(gasolina_aditivada_price) postoFields.gasolina_aditivada_price = gasolina_aditivada_price;
-    if(etanol_price) postoFields.etanol_price = etanol_price;
-    if(gnv_price) postoFields.gnv_price = gnv_price;
-    if(diesel_price) postoFields.diesel_price = diesel_price;
-    if(alcool_price) postoFields.alcool_price = alcool_price;
+        const postoFields = {};
+        if (name) postoFields.name = name;
+        if (address) postoFields.address = address;
+        if (gasolina_comun_price) postoFields.gasolina_comun_price = gasolina_comun_price;
+        if (gasolina_aditivada_price) postoFields.gasolina_aditivada_price = gasolina_aditivada_price;
+        if (etanol_price) postoFields.etanol_price = etanol_price;
+        if (gnv_price) postoFields.gnv_price = gnv_price;
+        if (diesel_price) postoFields.diesel_price = diesel_price;
+        if (alcool_price) postoFields.alcool_price = alcool_price;
 
-    try {
-        let posto = await Posto.findById(req.params.id);
-        if(!posto) {
-            return res.status(404).json({ msg: "Posto Não encontrado" });
+        try {
+            let posto = await Posto.findById(req.params.id);
+            if (!posto) {
+                return res.status(404).json({ msg: "Posto Não encontrado" });
+            }
+
+            posto = await Posto.findByIdAndUpdate(
+                req.params.id,
+                { $set: postoFields },
+                { new: true }
+            );
+
+            res.json(posto);
+        } catch (err) {
+            console.log(err.message);
+            res.status(500).json({ msg: "Server Error" })
         }
-
-        posto = await Posto.findByIdAndUpdate(
-            req.params.id,
-            {$set: postoFields},
-            {new: true}
-        );
-
-        res.json(posto);
-    } catch (err) {
-        console.log(err.message);
-        res.status(500).json({msg: "Server Error" })
-    }
-});
+    });
 
 // @route       DELETE api/postos
 // @desc        Remover um posto
@@ -167,14 +167,14 @@ router.put('/:id',
 router.delete('/:id', async (req, res) => {
     try {
         let posto = await Posto.findById(req.params.id);
-        if(!posto) {
+        if (!posto) {
             return res.status(404).json({ msg: "Posto Não encontrado" });
         }
         await Posto.findByIdAndDelete(req.params.id)
-        res.status(200).json({msg: "Posto Removido com sucesso" })
+        res.status(200).json({ msg: "Posto Removido com sucesso" })
     } catch (err) {
         console.log(err.message);
-        res.status(500).json({msg: "Server Error" })
+        res.status(500).json({ msg: "Server Error" })
     }
 });
 
