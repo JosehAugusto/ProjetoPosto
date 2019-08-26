1744ac11a432a480767b1af84f7ece835bd5cb91
Lucas Novaes <lucasmsn200@gmail.com>
2019-08-26 00:31:25 -0300

    preparando pra deploy

--- server.js
+++ server.js
@@ -1,19 +1,28 @@
 const express = require('express');
 const connectDB = require('./config/db')
+const path = require('path');
+
 const app = express();
 
 // Connect Database
 connectDB();
 
 // Init Middleware
-app.use(express.json({ extended: false}));
-
-app.get('/', (req, res) => res.json({msg: 'Hello User'}));
+app.use(express.json({ extended: false }));
 
 // Rotas
 app.use('/api/users', require('./routes/users'));
 app.use('/api/postos', require('./routes/postos'));
 app.use('/api/auth', require('./routes/auth'));
+
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  );
+}
 
 
 const PORT = process.env.PORT || 5000;
